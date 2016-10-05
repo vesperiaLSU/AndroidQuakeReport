ca239a796a212afb3ff15ebb91a7f988da676616
Chen Guo <guochen1223@gmail.com>
2016-10-05 16:56:08 -0500

    async task with http connection

--- app/src/main/java/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/android/quakereport/EarthquakeActivity.java
@@ -2,24 +2,48 @@ package android.quakereport;
 
 import android.content.Intent;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.util.StringBuilderPrinter;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.List;
 
 public class EarthquakeActivity extends AppCompatActivity {
+
+    /**
+     * Sample JSON response for a USGS query
+     */
+    private static final String LOG_TAG = EarthquakeActivity.class.getSimpleName();
+
+    /**
+     * URL to query the USGS dataset for earthquake information
+     */
+    private static final String USGS_REQUEST_URL =
+            "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2012-01-01&endtime=2012-12-01&minmagnitude=6";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
 
-        // Get the list of earthquakes from {@link QueryUtils}
-        List<Earthquake> earthquakes = QueryUtils.extractEarthquakes();
+        EarthquakeAsync task = new EarthquakeAsync();
+        task.execute(USGS_REQUEST_URL);
+    }
 
+    private void updateUI(List<Earthquake> earthquakes) {
         // Find a reference to the {@link ListView} in the layout
         ListView earthquakeListView = (ListView) findViewById(R.id.list);
 
@@ -50,5 +74,114 @@ public class EarthquakeActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    private class EarthquakeAsync extends AsyncTask<String, Void, List<Earthquake>> {
+
+        @Override
+        protected List<Earthquake> doInBackground(String... urls) {
+            if (urls.length == 0 || urls[0] == null) {
+                return null;
+            }
+
+            // Create URL object from the string provided
+            URL url = createUrl(urls[0]);
+
+            // Perform HTTP request to the URL and receive a JSON response back
+            String jsonResponse = "";
+            try {
+                jsonResponse = makeHttpRequest(url);
+            } catch (IOException exception) {
+                Log.e(LOG_TAG, "Error with making Http request", exception);
+            }
+
+            return QueryUtils.extractEarthquakes(jsonResponse);
+        }
+
+        @Override
+        protected void onPostExecute(List<Earthquake> earthquakes) {
+            if (earthquakes != null) {
+                updateUI(earthquakes);
+            }
+        }
+
+        /**
+         * Make Http request with the URL provided
+         *
+         * @param url the URL object to make connection to
+         * @return a string representing the json response
+         * @throws IOException
+         */
+        private String makeHttpRequest(URL url) throws IOException {
+            HttpURLConnection urlConnection = null;
+            InputStream inputStream = null;
+            String jsonResponse = "";
+
+            try {
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.setReadTimeout(10000 /*milliseconds*/);
+                urlConnection.setConnectTimeout(15000 /*milliseconds*/);
+                urlConnection.connect();
+
+                if (urlConnection.getResponseCode() == 200) {
+                    inputStream = urlConnection.getInputStream();
+                    jsonResponse = readFromInputStream(inputStream);
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+
+                if (inputStream != null) {
+                    inputStream.close();
+                }
+            }
+
+            return jsonResponse;
+        }
+
+        /**
+         * Read from the input stream and turns into a json response
+         *
+         * @param inputStream the input stream received from url connection
+         * @return a string representing the json response
+         * @throws IOException
+         */
+        private String readFromInputStream(InputStream inputStream) throws IOException {
+            StringBuilder sb = new StringBuilder();
+            if (inputStream != null) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String line = bufferedReader.readLine();
+                while (line != null && line.length() > 0) {
+                    sb.append(line);
+                    line = bufferedReader.readLine();
+                }
+            }
+
+            return sb.toString();
+        }
+
+        /**
+         * Create a URL object from string
+         *
+         * @param urlStr a string representing url
+         * @return a URL object
+         */
+        private URL createUrl(String urlStr) {
+            URL url;
+            try {
+                url = new URL(urlStr);
+            } catch (MalformedURLException exception) {
+                Log.e(LOG_TAG, "Error with creating URL", exception);
+                return null;
+            }
+
+            return url;
+        }
     }
 }
