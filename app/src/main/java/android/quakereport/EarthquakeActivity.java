20cd198996cf6130fa58f9a8685cf28faa7d2e3a
Chen Guo <guochen1223@gmail.com>
2016-10-10 11:11:46 -0500

    menu setting

--- app/src/main/java/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/android/quakereport/EarthquakeActivity.java
@@ -9,6 +9,8 @@ import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -39,6 +41,24 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderManag
      * Adapter for the list of earthquakes
      */
     private EarthquakeAdapter adapter;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_setting) {
+            Intent settingIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingIntent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
     /**
      * ListView for displaying the earthquakes
